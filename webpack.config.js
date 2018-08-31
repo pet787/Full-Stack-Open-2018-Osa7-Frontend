de88c9f1657b1ca3728b713742fee3dbd1e41c3d
pet787 <41019198+pet787@users.noreply.github.com>
2018-08-31 17:01:52 +0300

    7.14

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  }
+}
+module.exports = config
